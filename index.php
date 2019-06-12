1322ff899936600be2b371f5939e40b8db7fa8b3
websoftuz <48685621+websoftuz@users.noreply.github.com>
2019-06-12 12:51:36 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Forma</title>
+</head>
+<body>
+  <form action="index.php" method="POST">
+    Ismingiz: <input type="text" name="name"><br>
+    Pochtangiz: <input type="text" name="email"><br>
+    <input type="submit" name="submit" value="Jo'natish">
+  </form>
+</body>
+</html>
 <?php 
-echo "Salom";
-?>
+ $name = $_POST['name'];
+ $email = $_POST['email'];
+
+ echo "Salom ".$name;
